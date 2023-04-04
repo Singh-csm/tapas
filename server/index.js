6bff8e238ebb9e6964e975510cd090b1eeefe988
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-04-05 01:02:50 +0530

    changes  done

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 //==========CONFIGURATIONS======
 const __filename = fileURLToPath(import.meta.url); // to grab the file url , this only when we use type modules
@@ -45,6 +46,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 //========ROUTES=======
 app.use("/auth", authRoutes);
+app.use("/users",userRoutes);
 
 //=========MONGOOSE SETUP===
 const PORT = process.env.PORT || 6001;
---
+++ server/routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {getUser,getUserFriends,addRemoveFriend} from "../controllers/users.js";
+import {verifyToken} from "../middleware/auth.js";
+
+const router = express.Router();
+
+//=======READ======
+router.get("/:id",verifyToken,getUser);
+router.get("/:id",verifyToken,getUserFriends);
